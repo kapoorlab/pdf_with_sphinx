10b4c3588f510e005663f349580dc5c81bd01ba4
kapoorlab <randomaccessiblekapoor@gmail.com>
2025-09-04 14:51:39 +0200

    add latex and code

--- src/pdf_with_sphinx/publisher/build_paper.py
+++ src/pdf_with_sphinx/publisher/build_paper.py
@@ -77,7 +77,7 @@ def rst2tex(in_path, out_path):
         "use_verbatim_when_possible": True,
         "use_latex_citations": True,
         "latex_preamble": preamble,
-        "documentoptions": "letterpaper,compsoc,twoside",
+        "documentoptions": "letterpaper,compsoc,twoside,onecolumn",
         "halt_level": 3,  # 2: warn; 3: error; 4: severe
     }
 
